e69f7a382452c4bcd0e74de7aa04edc866d36d91
Ivan Lerner <lerner1796@gmail.com>
2019-12-18 08:47:38 +0000

    Init

---
+++ .bash_history
@@ -0,0 +1,28 @@
+ls
+pwd
+npx
+npx create nuxt-app
+npx create-nuxt-app
+ls
+npm i 
+npm run dev
+npm i
+exit
+npm i
+npm fund
+npm run dev
+ls
+exit
+ls
+npm run build
+npm i
+exit
+npm run build
+exit
+ls
+vim layouts/default.vue 
+npm run build
+exit
+vim pages/index.vue 
+npm run build
+exit
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card>
+        <v-card-title class="headline">
+        	Сайт в разработке
+	</v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
